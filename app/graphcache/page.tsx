8c089fa40133948f0de0cdb20fb0325cb10236bb
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-07-04 15:43:34 +0300

    remove metadata

--- app/graphcache/page.tsx
+++ app/graphcache/page.tsx
@@ -1,17 +1,6 @@
 import Pokemons  from "./_components/Pokemons";
 import { PokemonsQuery, createUrqlClient, testCache } from "./_components/lib";
 
-export const generateMetadata = async () => {
-  const [client] = createUrqlClient();
-
-  // const { data } = await client.query(PokemonsQuery).toPromise();
-
-  return {
-    title: 'Pokemons',
-    description: 'A list of pokemons',
-  };
-}
-
 export default function Page() {
   return (
     <Pokemons />
